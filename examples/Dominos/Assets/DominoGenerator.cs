76f59db63ab06906b3a5e731a6e80e7a5d59d195
Mike Treanor <treanor@american.edu>
2023-09-12 22:47:35 -0400

    Added dominos example from 9-12-23

---
+++ examples/Dominos/Assets/DominoGenerator.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoGenerator : MonoBehaviour
+{
+    // Remember to assign this value in the Unity editor!
+    public GameObject dominoPrefab;
+
+    // This is how many dominos we'll generate
+    int numDominos = 100;
+
+    // This is the distance we will put between each domino we generate.
+    float spacing = 0.8f;
+
+    GameObject firstDomino;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numDominos; i++)
+        {
+            // Compute a position in front of the generator object at a distance
+            // based on the iterator variable and the spacing variable.
+            Vector3 pos = transform.position + transform.forward * i * spacing;
+
+            // Instantiate the domino prefab into a game object at the location
+            GameObject domino = Instantiate(dominoPrefab, pos, transform.rotation);
+
+            // Let the instantiated domino know which one it is in the generated
+            // row. We can use this to create interesting effects, like
+            // determining what color it should be (see the DominoScript).
+            domino.GetComponent<DominoScript>().id = i;
+
+            // If this is the first domino created, store a reference to it in a
+            // variable. We can then apply a force to it to start the dominos
+            // falling (see Update below).
+            if (i == 0)
+            {
+                firstDomino = domino;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the spacce bar is pressed, apply a force to the first domino
+        // beginning the chain of dominos falling.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody rb = firstDomino.GetComponent<Rigidbody>();
+            rb.AddForce(firstDomino.transform.forward * 1000);
+        }
+    }
+}
---
+++ examples/Dominos/Assets/DominoScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoScript : MonoBehaviour
+{
+    public int id;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This function is called by Unity when the Physics object collides with
+    // something (because Domino has a Rigidbody).
+    private void OnCollisionEnter(Collision collision)
+    {
+        // The parameter 'collision' is a data structure provided by Unity
+        // and it contains information about the collision that occured.
+        // It also contains a reference to the gameObject that we collided with.
+        if (collision.gameObject.CompareTag("domino"))
+        {
+            // Here we will change the color to a 'color of the rainbow' based
+            // on this domino's id (id was set to be whatever number in order
+            // the domino was when it was created.
+            Renderer rend = gameObject.GetComponentInChildren<Renderer>();
+            // The Color.HSVToRGB function wants three values between 0 and 1.
+            // By dividing the number by 50 and applying the modulus operator
+            // we get a looping number between 0 and 1 every 50 id numbers.
+            rend.material.color = Color.HSVToRGB((id / 50f) % 1f, 0.8f, 0.8f);
+        }
+    }
+}
