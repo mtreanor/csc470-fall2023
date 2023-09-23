64b589af2279544630c953e8ae3aeef89d91d09d
Mike Treanor <treanor@american.edu>
2023-09-22 20:44:21 -0400

    Update BallScript.cs

--- examples/Basics-2/Assets/BallScript.cs
+++ examples/Basics-2/Assets/BallScript.cs
@@ -9,6 +9,8 @@ public class BallScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // As me mentioned in class, this is *one* problematic way of getting a
+        // reference to a GameManager script. We'll cover another way soon.
         GameObject gmObj = GameObject.Find("GameManagerObject");
         gm = gmObj.GetComponent<GameManager>();
     }
@@ -19,8 +21,17 @@ public class BallScript : MonoBehaviour
         
     }
 
+    // Unity will tell the function below to run under the following conditions:
+    //  1. Two objects with colliders are colliding
+    //  2. At least one of the objects' colliders are marked as "Is Trigger"
+    //  3. At least one of the objects has a Rigidbody
     private void OnTriggerEnter(Collider other)
     {
+        // 'other' is the name of the collider that just collided with the object
+        // that this script ("PlayerController") is attached to. So the if statment
+        // below checks to see that that object has the tag "coin". Remember that
+        // the tags for GameObjects are assigned in the top left area of the
+        // inspector when you select the obect.
         if (other.CompareTag("score"))
         {
             gm.incScore(1);
